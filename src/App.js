37cffa386375db9c487d94255075a3d82877b05c
Feleky Mirkó <skylc044@skylc.local>
2019-04-05 15:14:28 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,67 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const formSubmit = (value, setValue) => {
+    console.log("Submitted", value)
+    setValue('')
 }
 
-export default App;
+const ToDoFormSubmit = (todos, setTodos, value, setValue) => {
+    console.log("Submitted", value)
+    setTodos([...todos, { 'text': value }])
+    setValue('')
+}
+
+const deleteToDO = (index, todos, setTodos) => {
+    const newToDos = [...todos]
+    newToDos.splice(index, 1)
+    setTodos(newToDos)
+}
+
+const UseStateExample = () => {
+    const [count, setCount] = useState(0);
+    const [fruit, setFruit] = useState('');
+    const [value, setValue] = useState('');
+    const [todos, setTodos] = useState([{ text: 'Learn Hooks' }, { text: 'Learn Context' }, { text: 'Learn to drive' }]);
+    console.log(todos)
+    return (
+        <div>
+            <p>Counter: {count}</p>
+            <button onClick={() => setCount(count - 1)}>
+                Click me -1
+            </button>
+            <button onClick={() => setCount(count + 1)}>
+                Click me +1
+            </button>
+            <hr />
+
+            <p>Favourite fruit: {fruit}</p>
+            <form onSubmit={e => {
+                e.preventDefault()
+                formSubmit(fruit, setFruit)
+            }}>
+                <label>
+                    Fruit:
+                <input type="text" name="fruit" value={fruit} onChange={(event) => setFruit(event.target.value)} />
+                </label>
+                <input type="submit" name="fruitSubmitButton" value="Submit / Reset" />
+            </form>
+            <hr />
+
+            <ul>
+                {todos.map((todo, index) => <li key={index}>{todo.text}<button onClick={() => deleteToDO(index, todos, setTodos)}>X</button></li>)}
+            </ul>
+            <form onSubmit={e => {
+                e.preventDefault()
+                ToDoFormSubmit(todos, setTodos, value, setValue)
+            }}>
+                <label>
+                    ToDo:
+                <input type="text" name="todoInput" value={value} onChange={(event) => setValue(event.target.value)} />
+                </label>
+                <input type="submit" name="todoSubmitButton" value="Add" />
+            </form>
+        </div>
+    );
+}
+
+export default UseStateExample
